588932d4afd26427e1389419b5bb5abc74ddd6c6
anant2961 <anant2961@gmail.com>
2024-09-08 10:44:43 +0530

    changes staged

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -4,17 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDIjQ0pZlwXSfzkxg06Gi-jDgnh3SXW6fc",
-  authDomain: "ecommerce-80d80.firebaseapp.com",
-  projectId: "ecommerce-80d80",
-  storageBucket: "ecommerce-80d80.appspot.com",
-  messagingSenderId: "27915653010",
-  appId: "1:27915653010:web:ba174926c1ec1638223b4b",
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
 };
 
 // Initialize Firebase
