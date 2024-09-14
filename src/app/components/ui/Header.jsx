45b1e58a8be45fdf585229885207177cca36c088
anant2961 <anant2961@gmail.com>
2024-09-14 05:37:29 +0530

    searchBar width increased

--- src/app/components/ui/Header.jsx
+++ src/app/components/ui/Header.jsx
@@ -20,7 +20,7 @@ export default async function Header() {
   const res = await fetch(endpoint);
   const product = await res.json();
   return (
-    <div className="w-full bg:whiteText">
+    <div className="w-full bg:whiteText z-50">
       <div className="max-w-screen-xl mx-auto h-20 flex items-center justify-between px-4 py-2 lg:px-0">
         <Hamburger />
         <Link href="/">
--- src/app/components/ui/SearchBar.jsx
+++ src/app/components/ui/SearchBar.jsx
@@ -14,27 +14,28 @@ const SearchBar = ({ products }) => {
   }, [searchText]);
 
   return (
-    <div className=" flex md:inline-flex w-full max-w-3xl relative ml-4 mr-4 ">
-      <input
-        type="text"
-        onChange={(e) => setSearchText(e.target.value)}
-        value={searchText}
-        placeholder="Search for products..."
-        className="w-2/6 flex-1 rounded-xl  text-gray-900 text-lg placeholder:text-base placeholder:tracking-normal shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:font-normal focus:ring-1 focus:ring-darkText sm:text-sm px-10 py-2"
-      />
-      <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-        {searchText ? (
-          <X
-            onClick={() => setSearchText("")}
-            className="text-md hover:text-red-500 cursor-pointer duration-200"
-          />
-        ) : (
-          <Search className="text-md" />
-        )}
+    <>
+      <div className=" flex md:inline-flex w-full max-w-3xl relative ml-4 mr-4 ">
+        <input
+          type="text"
+          onChange={(e) => setSearchText(e.target.value)}
+          value={searchText}
+          placeholder="Search for products..."
+          className="w-full flex-1 rounded-xl  text-gray-900 text-lg placeholder:text-base placeholder:tracking-normal shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:font-normal focus:ring-1 focus:ring-darkText sm:text-sm px-10 py-2"
+        />
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+          {searchText ? (
+            <X
+              onClick={() => setSearchText("")}
+              className="text-md hover:text-red-500 cursor-pointer duration-200"
+            />
+          ) : (
+            <Search className="text-md" />
+          )}
+        </div>
       </div>
-      {/* Search product will go here */}
       {searchText && (
-        <div className="absolute left-0 top-20 w-full mx-auto max-h-[500px] px-10 py-5 bg-white z-20 overflow-y-scroll text-black shadow-lg shadow-skyText scrollbar-hide">
+        <div className="absolute left-0 top-20 w-full mx-auto max-h-[500px] px-10 py-5 bg-white z-20 overflow-y-scroll text-black shadow-lg shadow-skyText scrollbar:hidden">
           {searchResults.length > 0 ? (
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5">
               {searchResults?.map((item) => (
@@ -56,7 +57,7 @@ const SearchBar = ({ products }) => {
           )}
         </div>
       )}
-    </div>
+    </>
   );
 };
 
